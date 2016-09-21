95539284e7baee0adfee0be155562aac25a734b4
carlosvillar76 <cvillarce15dw@ikzubirimanteo.com>
2016-09-21 06:39:30 +0000

    Añadido ver_listas.php

--- conexion.php
+++ conexion.php
@@ -7,10 +7,10 @@ $pass = "";
 
 try {
     $dbh = new PDO('mysql:host=localhost;dbname=TODO_LIST', $user, $pass);
-    }
-    $dbh = null;
+    
 } catch (PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
+
 ?>
--- ver_listas.php
+++ ver_listas.php
@@ -5,18 +5,14 @@ require_once("conexion.php");
 try {
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	$stmt = $db->prepare('SELECT * FROM listas');
 	
-	if ($userid === false) {
-		echo "Error: not INPUT_GET userid";
-	} else {
-		$stmt->bindParam(':userid', $userid, PDO::PARAM_INT); // <-- Automatically sanitized for SQL by PDO
-		$stmt->execute();
-		$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-		echo json_encode($result);
-	}
+	$stmt = $db->prepare('SELECT * FROM listas');
+	$stmt->execute();
+	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($result);
 	
 } catch (PDOException $e) {
 	echo $e->getMessage();
 }
+
 ?>
